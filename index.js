61ca78914c0f84106d1dfdc87ee6d8b22bfc0304
hardikmodi5867 <hardikmodi58@gmail.com>
2023-12-23 03:42:24 +0530

    demo for various image manipulation tasks using @imagely/remove-background-node

---
+++ index.js
@@ -0,0 +1,87 @@
+const { removeBackground,removeForeground,segmentForeground } = require("@imgly/background-removal-node")
+const Jimp = require("jimp")
+const sharp = require('sharp');
+
+const fs = require("fs")
+async function removeBg(ipFileName) {
+	try {
+		const opFileName = "output_bg_removed.png"
+		const fileBlob = await removeBackground(ipFileName,{
+      publicPath: "file://"+__dirname+"/model_files/",
+    })
+		const arrayBuffer = await fileBlob.arrayBuffer()
+		fs.writeFileSync(opFileName, Buffer.from(arrayBuffer))
+
+    const opacity = 0.5
+    const image = await Jimp.read(opFileName)
+    image.opacity(opacity)
+    return new Promise((resolve,reject) => {
+      image.write(opFileName, (err) => {
+        if(err) reject(err)
+        resolve(opFileName)
+      })
+    })
+    
+	} catch (err) {
+		console.error(`Something went wrong in removeBg: ${err.message}`,err)
+	}
+};
+
+
+async function removeFg(ipFileName) {
+	try {
+		let opFileName = "output_fg_removed.png"
+		const fileBlob = await removeForeground(ipFileName,{
+      publicPath: "file://"+__dirname+"/model_files/",
+      output:{
+        quality: 0.8,
+        format: "image/jpeg",
+      }
+    })
+		const arrayBuffer = await fileBlob.arrayBuffer()
+		fs.writeFileSync(opFileName, Buffer.from(arrayBuffer))
+
+    const opacity = 0.5
+    const image = await Jimp.read(opFileName)
+    image.opacity(opacity)
+
+    opFileName = `${opFileName.split(".")[0]}_${opacity}.png`
+
+    return new Promise((resolve,reject) => {
+      image.write(opFileName, (err) => {
+        if(err) reject(err)
+        resolve(opFileName)
+      })
+    })
+
+	} catch (err) {
+		console.error(`Something went wrong in removeFg: ${err.message}`,err)
+	}
+};
+
+async function combineImages(foregroundImage,backgroundImage) {
+  try {
+    let opFileName = "output.png"
+    return new Promise((resolve,reject) => {
+      sharp(backgroundImage).composite([{input: foregroundImage, gravity: 'center' }]).toFile(opFileName, (err, info) => {
+        if(err) reject(err)
+        resolve(opFileName)
+      });
+    })
+  } catch (error) {
+    console.error(`Something went wrong in combineImages: ${error.message}`,error)
+  }
+}
+
+
+(async () => {
+  const ipFileName = "input.jpeg"
+  const [foregroundImage,backgroundImage] = await Promise.all([removeBg(ipFileName),removeFg(ipFileName)])
+  const opFileName = await combineImages(foregroundImage,backgroundImage)
+  console.info(`Output file: ${opFileName}`)
+  fs.readdirSync(__dirname).forEach(file => {
+    if(file.endsWith(".png") && !["output.png","input.png"].includes(file)) {
+      fs.unlinkSync(file)
+    }
+  })
+})()
---
+++ readme.md
@@ -0,0 +1,40 @@
+# Image Manipulation Repository
+
+This repository contains code for various image manipulation tasks such as:
+
+- Removing the background from an image
+- Removing the foreground from an image
+- Reducing the opacity of an image
+- Combining multiple images
+
+## Getting Started
+
+To get started with this project, clone the repository and install the dependencies.
+
+```sh
+git clone https://github.com/hardikmodi58677/image-bg-fg_removal_demo.git
+cd image-bg-fg_removal_demo
+npm install
+```
+
+## Usage
+The main entry point of the application is index.js. You can run it with Node.js:
+
+```sh
+node index.js
+```
+
+This project uses npm scripts for managing workflow. Here is a basic guide on using these scripts:
+
+- `npm run start` - Runs the application
+
+## Contributing
+Contributions are welcome. Please open up an issue or create PR if you would like to help out.
+
+## Note
+This project is still in development. Use it at your own risk.
+
+## License
+License
+MIT
+
